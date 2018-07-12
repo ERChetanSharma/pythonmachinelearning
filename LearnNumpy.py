504bdb5bc39eb1c2b343a3a7096746e74f3ea622
Chetan Sharma <chetan.sharma19971@outlook.com>
2018-07-12 13:52:28 +0530

    Numpy Content

---
+++ LearnNumpy.py
@@ -0,0 +1,63 @@
+import numpy
+import sys
+var=numpy.array([(1,2,3),(2,3,4)])
+# print(var)
+# print(var.min())
+# print(var.max())
+# print(var.sum())
+
+
+# range() vs arange()
+
+
+# r=range(100)
+# print(r)
+#
+# d=numpy.arange(100)
+# print(d)
+
+# # properties
+# print(var)
+# print(var.ndim)
+# print(var.itemsize)
+# print(var.shape)
+# print(var.dtype)
+
+# intervals
+# print(numpy.linspace(0,3,7))
+
+# print(var)
+# print()
+# print(var[0][1])
+
+# reshape of matrix
+# print(var.reshape(3,2))
+# print(var)
+# print()
+#
+#
+# # combing of two arrays
+var1=numpy.array([(1,2,3),(2,3,4)])
+#
+# print(numpy.hstack(var))
+# print()
+# print(numpy.vstack(var1))
+
+#
+# print(numpy.sqrt(var))
+# print(numpy.std(var))
+
+# print(var)
+# print(numpy.ravel(var))
+print(var+var1)
+print(var-var1)
+print(var*var1)
+print(var/var1)
+
+
+
+
+
+
+
+
